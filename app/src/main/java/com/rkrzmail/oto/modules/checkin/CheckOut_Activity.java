15b2ac56f544a70808eb30bb1f28b89696690952
Khesa Alvandi Sembodo <44808111+masesas@users.noreply.github.com>
2021-01-03 02:36:58 +0700

    update

--- app/src/main/java/com/rkrzmail/oto/modules/checkin/CheckOut_Activity.java
+++ app/src/main/java/com/rkrzmail/oto/modules/checkin/CheckOut_Activity.java
@@ -1,9 +1,13 @@
 package com.rkrzmail.oto.modules.checkin;
 
+import android.annotation.SuppressLint;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -12,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.naa.data.Nson;
 import com.naa.utils.InternetX;
@@ -19,58 +24,71 @@ import com.naa.utils.Messagebox;
 import com.rkrzmail.oto.AppActivity;
 import com.rkrzmail.oto.AppApplication;
 import com.rkrzmail.oto.R;
+import com.rkrzmail.oto.modules.BarcodeActivity;
 import com.rkrzmail.srv.NikitaRecyclerAdapter;
 import com.rkrzmail.srv.NikitaViewHolder;
 
 import java.util.Map;
 
+import static com.rkrzmail.utils.APIUrls.CHECKOUT;
+import static com.rkrzmail.utils.APIUrls.VIEW_SPAREPART;
+import static com.rkrzmail.utils.ConstUtils.ERROR_INFO;
+import static com.rkrzmail.utils.ConstUtils.REQUEST_BARCODE;
+
 public class CheckOut_Activity extends AppActivity {
 
     private RecyclerView rvCheckout;
+    private int checkinId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_out_);
+        setContentView(R.layout.activity_list_basic);
         initComponent();
     }
 
     private void initToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_checkout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Check-Out");
+        getSupportActionBar().setTitle("Check Out");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void initComponent() {
         initToolbar();
 
-        rvCheckout = findViewById(R.id.recyclerView_checkout);
+        rvCheckout = findViewById(R.id.recyclerView);
         rvCheckout.setLayoutManager(new LinearLayoutManager(this));
         rvCheckout.setAdapter(new NikitaRecyclerAdapter(nListArray, R.layout.item_checkout) {
                     @Override
                     public void onBindViewHolder(@NonNull final NikitaViewHolder viewHolder, final int position) {
                         super.onBindViewHolder(viewHolder, position);
 
-                        viewHolder.find(R.id.tv_jenis_checkout, TextView.class).setText(nListArray.get(position).get("").asString());
-                        viewHolder.find(R.id.tv_nopol_checkout, TextView.class).setText(nListArray.get(position).get("").asString());
-                        viewHolder.find(R.id.tv_namaP_checkout, TextView.class).setText(nListArray.get(position).get("").asString());
-                        viewHolder.find(R.id.tv_noKunci_checkout, TextView.class).setText(nListArray.get(position).get("").asString());
+                        viewHolder.find(R.id.tv_jenis_checkout, TextView.class).setText(nListArray.get(position).get("JENIS_KENDARAAN").asString());
+                        viewHolder.find(R.id.tv_nopol_checkout, TextView.class).setText(nListArray.get(position).get("NOPOL").asString());
+                        viewHolder.find(R.id.tv_namaP_checkout, TextView.class).setText(nListArray.get(position).get("NAMA_PELANGGAN").asString());
+                        viewHolder.find(R.id.tv_noKunci_checkout, TextView.class).setText(nListArray.get(position).get("NO_KUNCI").asString());
 
                     }
                 }.setOnitemClickListener(new NikitaRecyclerAdapter.OnItemClickListener() {
                     @Override
                     public void onItemClick(Nson parent, View view, int position) {
-//                        Intent i = new Intent(getActivity(), DetailKontrolLayanan_Activity.class);
-//                        i.putExtra("data", nListArray.get(position).toJson());
-//                        startActivity(i);
+                        checkinId = parent.get(position).get("ID").asInteger();
+                        startActivityForResult(new Intent(getActivity(), BarcodeActivity.class), REQUEST_BARCODE);
                     }
                 })
         );
-        catchData("");
+
+        viewCheckout("", "N");
+        find(R.id.swiperefresh, SwipeRefreshLayout.class).setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                viewCheckout("", "N");
+            }
+        });
     }
 
-    private void catchData(final String cari) {
+    private void viewCheckout(final String cari, final String barcode) {
         newProses(new Messagebox.DoubleRunnable() {
             Nson result;
 
@@ -79,8 +97,10 @@ public class CheckOut_Activity extends AppActivity {
                 Map<String, String> args = AppApplication.getInstance().getArgsData();
                 args.put("action", "view");
                 args.put("search", cari);
-
-                result = Nson.readJson(InternetX.postHttpConnection(AppApplication.getBaseUrlV3(""), args));
+                if(barcode.equals("Y")){
+                    args.put("checkinId", String.valueOf(checkinId));
+                }
+                result = Nson.readJson(InternetX.postHttpConnection(AppApplication.getBaseUrlV3(CHECKOUT), args));
             }
 
             @Override
@@ -128,7 +148,7 @@ public class CheckOut_Activity extends AppActivity {
             public boolean onQueryTextSubmit(String query) {
                 searchMenu.collapseActionView();
                 //filter(null);
-                catchData(query);
+                viewCheckout(query, "N");
 
                 return true;
             }
@@ -137,4 +157,12 @@ public class CheckOut_Activity extends AppActivity {
         return true;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_BARCODE) {
+            //data != null ? Integer.parseInt(data.getStringExtra("TEXT").replace("\n", "").trim()) : 0
+            viewCheckout("", "Y");
+        }
+    }
 }
