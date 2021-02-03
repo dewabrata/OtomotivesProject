c9e5d3c45a3013311ff8995c274afb773802984a
Ednan <ednanm22@gmail.com>
2021-02-04 01:14:28 +0700

    update

--- app/src/main/java/com/rkrzmail/oto/modules/bengkel/SaranActivity.java
+++ app/src/main/java/com/rkrzmail/oto/modules/bengkel/SaranActivity.java
@@ -1,15 +1,134 @@
 package com.rkrzmail.oto.modules.bengkel;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import com.naa.data.Nson;
+import com.naa.utils.InternetX;
+import com.naa.utils.Messagebox;
+import com.rkrzmail.oto.AppActivity;
+import com.rkrzmail.oto.AppApplication;
 import com.rkrzmail.oto.R;
+import com.rkrzmail.oto.modules.komisi.AturKomisiLayanan_Activity;
+import com.rkrzmail.oto.modules.sparepart.AturTerimaPart_Activity;
+import com.rkrzmail.oto.modules.sparepart.DetailTerimaPart_Activity;
+import com.rkrzmail.srv.NikitaRecyclerAdapter;
+import com.rkrzmail.srv.NikitaViewHolder;
+import com.rkrzmail.utils.Tools;
 
-public class SaranActivity extends AppCompatActivity {
+import java.util.Map;
+import java.util.Objects;
+
+import static com.rkrzmail.utils.APIUrls.VIEW_MST;
+import static com.rkrzmail.utils.APIUrls.VIEW_SARAN;
+import static com.rkrzmail.utils.ConstUtils.ADD;
+import static com.rkrzmail.utils.ConstUtils.PART;
+import static com.rkrzmail.utils.ConstUtils.REQUEST_DETAIL;
+import static com.rkrzmail.utils.ConstUtils.REQUEST_SARAN;
+import static com.rkrzmail.utils.ConstUtils.RP;
+import static java.nio.file.Files.find;
+
+public class SaranActivity extends AppActivity {
+
+    private RecyclerView recyclerView_saran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_basic_with_fab);
+        initComponent();
+        initToolbar();
+
+    }
+
+    @SuppressLint("NewApi")
+    private void initToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Saran");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initComponent(){
+        catchData("");
+        recyclerView_saran = findViewById(R.id.recyclerView);
+        find(R.id.fab_tambah).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), AturSaran_Activity.class);
+                i.putExtra(ADD, "");
+                startActivityForResult(i, REQUEST_SARAN);
+            }
+        });
+
+        find(R.id.swiperefresh, SwipeRefreshLayout.class).setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                catchData("");
+            }
+        });
+        initRecylerview();
+    }
+
+    private void initRecylerview(){
+        recyclerView_saran.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView_saran.setHasFixedSize(true);
+        recyclerView_saran.setAdapter(new NikitaRecyclerAdapter(nListArray, R.layout.item_saran){
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onBindViewHolder(@NonNull NikitaViewHolder viewHolder, int position) {
+                super.onBindViewHolder(viewHolder, position);
+                viewHolder.find(R.id.txtJudul, TextView.class).setText(nListArray.get(position).get("JUDUL_SARAN").asString());
+                viewHolder.find(R.id.txtTgl, TextView.class).setText(nListArray.get(position).get("DATEONLY").asString());
+                viewHolder.find(R.id.txtJam, TextView.class).setText(nListArray.get(position).get("TIMEONLY").asString());
+            }
+        });
+    }
+
+    private void catchData(final String cari) {
+        newProses(new Messagebox.DoubleRunnable() {
+            Nson result;
+
+            @Override
+            public void run() {
+                Map<String, String> args = AppApplication.getInstance().getArgsData();
+                args.put("action", "view");
+                args.put("search", cari);
+                result = Nson.readJson(InternetX.postHttpConnection(AppApplication.getBaseUrlV3(VIEW_SARAN), args));
+            }
+
+            @Override
+            public void runUI() {
+                if (result.get("status").asString().equalsIgnoreCase("OK")) {
+                    nListArray.asArray().clear();
+                    nListArray.asArray().addAll(result.get("data").asArray());
+                    recyclerView_saran.getAdapter().notifyDataSetChanged();
+                } else {
+                    showInfo("Gagal memuat Aktifitas");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK ) {
+            if (requestCode == REQUEST_SARAN) {
+                catchData("");
+            }
+        }
     }
 }
