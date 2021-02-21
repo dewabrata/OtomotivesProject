635b351b3205d26dc0ce7379e116eff4a9563ba2
ednanmaulana <poqqpoqq22>
2021-02-21 21:52:01 +0700

    update

---
+++ app/src/main/java/com/rkrzmail/oto/modules/mekanik/ClaimGaransiPart_Activity.java
@@ -0,0 +1,16 @@
+package com.rkrzmail.oto.modules.mekanik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rkrzmail.oto.AppActivity;
+import com.rkrzmail.oto.R;
+
+public class ClaimGaransiPart_Activity extends AppActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_claim_garansi_part_);
+    }
+}
